89078478f156d060603ec3a6a5b36bc426805adc
axel-paillaud <contact@axelweb.fr>
2024-04-12 07:18:47 +0200

    setup project structure, hierarchy folder

---
+++ js/Components/button/button.js
@@ -0,0 +1,7 @@
+import './button.css';
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    console.log("you click on me!");
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { myVariable } from './test.js';
+import '../assets/style.css';
+import './Components/button/button.js';
+
+console.log("hello, world!");
+
+console.log(myVariable);
