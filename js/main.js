15710cf0a227d1d612925786088a6879331041ba
axel-paillaud <contact@axelweb.fr>
2024-04-15 07:29:17 +0200

    add CSS base for three elements: header, title, cover

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import { myVariable } from './test.js';
+import '../assets/css/root.css';
 import '../assets/css/normalize.css';
 import '../assets/css/font.css';
 import '../assets/css/style.css';
+import './Components/header/header.js';
+import './Components/title/title.js';
+import './Components/cover/cover.js';
 
 console.log("hello, world!");
 
