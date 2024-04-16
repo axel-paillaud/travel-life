6f93e0c661cb38cea6da03899168c666012ef603
axel-paillaud <contact@axelweb.fr>
2024-04-16 06:48:11 +0200

    set img to full height and width for responsive

---
+++ js/importCss.js
@@ -0,0 +1,5 @@
+import '../assets/css/root.css';
+import '../assets/css/utility.css';
+import '../assets/css/normalize.css';
+import '../assets/css/font.css';
+import '../assets/css/style.css';
--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-import { myVariable } from './test.js';
-import '../assets/css/root.css';
-import '../assets/css/utility.css';
-import '../assets/css/normalize.css';
-import '../assets/css/font.css';
-import '../assets/css/style.css';
+import './importCss.js';
 import './Components/header/header.js';
 import './Components/title/title.js';
 import './Components/cover/cover.js';
 
 console.log("hello, world!");
 
-console.log(myVariable);
