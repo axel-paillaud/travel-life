9657fad20239242614b9f50aab22426eb6b8b554
axel-paillaud <contact@axelweb.fr>
2024-04-17 07:18:44 +0200

    add responsive adjustement block on mobile, to avoid big white middle space

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import './importCss.js';
 import './Components/header/header.js';
 import './Components/title/title.js';
 import './Components/cover/cover.js';
+import './Components/responsiveAdjustementBlock/responsiveAdjustementBlock.js';
 
 console.log("hello, world!");
 
