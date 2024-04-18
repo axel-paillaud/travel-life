a262a60fbecbf2061f00c4cd1161b0ff38c3cf1f
axel-paillaud <contact@axelweb.fr>
2024-04-18 06:57:33 +0200

    add white background css animation

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import './Components/header/header.js';
 import './Components/title/title.js';
 import './Components/cover/cover.js';
 import './Components/responsiveAdjustementBlock/responsiveAdjustementBlock.js';
+import './Components/backgroundAnimation/backgroundAnimation.js';
 
 console.log("hello, world!");
 
